3eaf8ead49989f195aab2e7b45cbc622c27c1cda
SeongJoon Seo <seongjoon.seo@sk.com>
2021-11-11 16:59:28 +0900

    step9. view model state 정의

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
@@ -16,22 +16,49 @@
 
 package com.codelabs.state.todo
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
 
-    private var _todoItems = MutableLiveData(listOf<TodoItem>())
-    val todoItems: LiveData<List<TodoItem>> = _todoItems
+    var todoItems = mutableStateListOf<TodoItem>()
+        private set
+
+    var currentEditPosition by mutableStateOf(INVALID_EDIT_POSITION)
+
+    val currentEditItem: TodoItem?
+        get() = todoItems.getOrNull(currentEditPosition)
 
     fun addItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!! + listOf(item)
+        todoItems.add(item)
     }
 
     fun removeItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!!.toMutableList().also {
-            it.remove(item)
+        todoItems.remove(item)
+        onEditDone()
+    }
+
+    fun onEditItemSelected(position: Int) {
+        currentEditPosition = position
+    }
+
+    fun onEditDone() {
+        currentEditPosition = INVALID_EDIT_POSITION
+    }
+
+    fun onEditItemChange(item: TodoItem) {
+        val currentItem = requireNotNull(currentEditItem)
+        require(currentItem.id == item.id) {
+            "You can only change an item with the same id as currentEditItem"
         }
+
+        todoItems[currentEditPosition] = item
+    }
+
+    companion object {
+        const val INVALID_EDIT_POSITION = -1
     }
 }
